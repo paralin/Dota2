46f88320f0ff4f44d2dd7abc720324bed62fbc16
sbx320 <sbx320@sbx320.net>
2016-08-09 01:22:41 +0200

    Updated SteamKit to 1.8.0 and NET Core to 1.0.0

--- src/Dota2/Base/Data/Specifications.cs
+++ src/Dota2/Base/Data/Specifications.cs
@@ -34,8 +34,6 @@ namespace Dota2.Base.Data
             {CSOTypes.ECON_ITEM, typeof(CSOEconItem)},
             {CSOTypes.ITEM_RECIPE, typeof(CSOItemRecipe)},
             {CSOTypes.ECON_GAME_ACCOUNT_CLIENT, typeof(CSOEconGameAccountClient)},
-            {CSOTypes.SELECTED_ITEM_PRESET, typeof(CSOSelectedItemPreset)},
-            {CSOTypes.ITEM_PRESET_INSTANCE, typeof(CSOEconItemPresetInstance)},
             {CSOTypes.DROP_RATE_BONUS, typeof(CSOEconItemDropRateBonus)},
             {CSOTypes.LEAGUE_VIEW_PASS, typeof(CSOEconItemLeagueViewPass)},
             {CSOTypes.EVENT_TICKET, typeof(CSOEconItemEventTicket)},
